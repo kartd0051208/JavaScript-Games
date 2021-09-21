728b25323b0076478f6b708b3938fb86da28d9b0
Eli YE <83023995+kartd0051208@users.noreply.github.com>
2021-09-21 21:13:05 +0800

    Delete app.js

--- app.js
+++
@@ -1,9 +0,0 @@
-// celsius to fahrenheit
-// T(ºC)x1.8 +32
-
-function convert(oc){
-    return oc*1.8+32;
-}
-let input = prompt("請輸入溫度");
-let result = convert(input);
-alert("換算後的溫度為" +result +"ºF"); 
